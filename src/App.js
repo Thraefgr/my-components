e2f0840d0ed87896bc35f958197419fe70779a70
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-11-09 23:55:14 +0300

    Add a card flip animation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import cardArt from "./card-art.png"
 
 function App() {
   return (
-    <div className="card">
-      <div className="card-front">
-        <div className="card-power">10</div>
-        <div className="card-name">First of The Cards</div>
-      </div>
-      <div className="card-back">
-        <p className="card-story">Once upon a time there was a wicked god...</p>
+    <div className="card-container">
+      <div className="card">
+        <div className="card-front">
+          <div className="card-power">10</div>
+          <div className="card-name">First of The Cards</div>
+        </div>
+        <div className="card-back">
+          <p className="card-story">Once upon a time there was a wicked card...</p>
+        </div>
       </div>
     </div>
   );
